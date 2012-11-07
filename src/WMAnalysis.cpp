ceb686f75e7ddb950b5c5be0610388a36c39f79a
Oliver Perks <ofjp@hpsg.dcs.warwick.ac.uk>
2012-11-07 13:52:03 +0000

    Included MPI Memory consumption calculation in Functional output

--- src/WMAnalysis.cpp
+++ src/WMAnalysis.cpp
@@ -42,30 +42,37 @@ void WMAnalysis::generateFunctionBreakdown(TraceReader * tr) {
 	set<FunctionSiteAllocation *, FunctionSiteAllocation::comparatorMem> call_sites_mem(
 			call_sites.begin(), call_sites.end());
 
-	/* Generate filename */
-	string hwm_filename = WMUtils::makeFunctionsFilename(trace_file_name);
-
-	/* Make file object */
-	ofstream hwm_file(hwm_filename.c_str());
+	/* Get High Water Mark */
 	long HWM = tr->getHWMMemory();
 
-	hwm_file << "# HWM Functions file from WMTools - " << hwm_filename
-			<< " HWM of " << HWM << "(B)\n";
-	hwm_file << "# High Water Mark Function Breakdown\n";
+	/* Make a temp string buffer for writing to */
+	stringstream temp_stream (stringstream::in | stringstream::out);
+
 
 	/* Establish Iterator */
 	set<FunctionSiteAllocation *, FunctionSiteAllocation::comparatorMem>::reverse_iterator it =
 			call_sites_mem.rbegin();
 
+	/* Track MPI Memory function consumption */
+	long mpi_memory = 0;
+	bool mpi_found = false;
+	string mpi_function_name("libmpi");
+	double mpi_memory_percentage = 0;
+
+
 	/* Reverse iterate over call sites > orderd by size, dumping to file */
 	while (it != call_sites_mem.rend()) {
+
+		/* Reset MPI lib found status */
+		mpi_found = false;
+
 		FunctionSiteAllocation * fsa = *it;
 		int stackID = fsa->getStackId();
 		double percentage = ((double) fsa->getMemory()) / HWM;
 		percentage *= 100;
 
 		/* Output stack ID + data */
-		hwm_file << "Call Stack: " << stackID << " Allocated "
+		temp_stream << "Call Stack: " << stackID << " Allocated "
 				<< fsa->getMemory() << "(B) (" << percentage << "(%) ) from "
 				<< fsa->getCount() << " allocations\n";
 
@@ -75,18 +82,48 @@ void WMAnalysis::generateFunctionBreakdown(TraceReader * tr) {
 			/* Make indentation */
 			int j;
 			for (j = 0; j < i; j++)
-				hwm_file << "-";
+				temp_stream << "-";
 
-			hwm_file << functions[i] << "\n";
+			temp_stream << functions[i] << "\n";
+
+			if(!mpi_found && functions[i].find(mpi_function_name)!=string::npos){
+				mpi_memory += fsa->getMemory();
+				mpi_found = true;
+			}
+
 		}
-		hwm_file << "\n\n";
+		temp_stream << "\n\n";
 
 		/* Move to next allocation site */
 		it++;
 	}
 
-	/* Close the file */
-	hwm_file.close();
 
+
+
+	/* Generate filename */
+	string hwm_filename = WMUtils::makeFunctionsFilename(trace_file_name);
+
+	/* Make file object */
+	ofstream hwm_file(hwm_filename.c_str());
+
+	hwm_file << "# HWM Functions file from WMTools - " << hwm_filename
+			<< " HWM of " << HWM << "(B)\n";
+	hwm_file << "#\n";
+
+	/* Dump MPI Memory to file */
+	mpi_memory_percentage = (((double) mpi_memory) / HWM)*100;
+	hwm_file << "# MPI Memory summary: " << mpi_memory << "(B) (" << mpi_memory_percentage << "%) of memory attributed to MPI (" << mpi_function_name << ")\n";
+	hwm_file << "#\n";
+	hwm_file << "# High Water Mark Function Breakdown\n";
+	hwm_file << "\n";
+
+
+	/* Copy contents of temp stream buffer to final file */
+	hwm_file<< temp_stream.str();
+
+
+	/* Close the files */
+	hwm_file.close();
 }
 
