58dc2a5c5c8b0febca723652a8b5e52db135af47
Oliver Perks <olly.perks@gmail.com>
2015-03-14 11:39:08 +0000

    Updated license to GPL, and small bug fix

--- src/util/TraceReader.cpp
+++ src/util/TraceReader.cpp
@@ -1,3 +1,23 @@
+/*
+* Copyright 2012 University of Warwick.
+*
+* This file is part of WMTools.
+*
+* WMTools is free software: you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or (at your option)
+* any later version.
+*
+* WMTools is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* WMTools. If not, see http://www.gnu.org/licenses/. 
+*/
+
+
 #include "../../include/util/TraceReader.h"
 
 TraceReader::TraceReader(string filename, bool consumptionGraph,
@@ -198,6 +218,7 @@ void TraceReader::processEvents() {
 		} else {
 			flag = frame_data->FINISHFLAG;
 			data_remaining--;
+                        return;
 		}
 
 		/* Check the alloc ID against the search */
